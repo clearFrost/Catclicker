c9fbca92f0a86441b59759169b5bf7d59a291c34
Jason Kang <kangjason02@gmail.com>
2023-02-05 22:03:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.helloworld
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.catButton)
+        button.setOnClickListener {
+            Log.v("Hello World", "Button Clicked!")
+            Toast.makeText(this, "Cat is hungry", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
